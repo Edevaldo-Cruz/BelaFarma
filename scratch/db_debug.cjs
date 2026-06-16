b8523ec9c52b6bc081e1fdb5857b164ad7f4c6a7
Edevaldo-Cruz <edevaldo_cruz@hotmail.com>
2026-06-16 16:49:48 -0300

    chore: remove db_debug.cjs

--- scratch/db_debug.cjs
+++
@@ -1,9 +0,0 @@
-const Database = require('better-sqlite3');
-const path = require('path');
-const db = new Database(path.join(__dirname, '../belafarma.db'));
-try {
-  const rows = db.prepare("SELECT * FROM scraped_images WHERE image_url IS NOT NULL LIMIT 5").all();
-  console.log("Images in DB:", rows);
-} catch (e) {
-  console.error("Error querying DB:", e);
-}
