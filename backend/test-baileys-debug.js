7234491430983bbb68da5d28f58351ceb5ac2f13
Edevaldo-Cruz <edevaldo_cruz@hotmail.com>
2026-08-03 10:24:05 -0300

    debug: add process.exit

--- backend/test-baileys-debug.js
+++ backend/test-baileys-debug.js
@@ -3,7 +3,9 @@ console.log('--- TESTANDO CONEXÃO BAILEYS ---');
 baileys.connect().then(() => {
   setTimeout(() => {
     console.log('STATUS ATUAL:', baileys.getStatus());
+    process.exit(0);
   }, 5000);
 }).catch(err => {
   console.error('ERRO CATCH:', err);
+  process.exit(1);
 });
